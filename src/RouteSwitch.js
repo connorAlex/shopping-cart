74b431515b8f1ff8e17f15db8baecedf086630d3
connorAlex <alex.connor.cochrane@gmail.com>
2022-10-19 08:47:10 -0500

    add routes to checkout and shop

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,11 +1,16 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import App from './App';
+import Checkout from './components/Checkout';
+import Shop from './components/Shop';
 
 const RouteSwitch = () => {
+
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<App />} />
+                <Route path='/checkout' element={<Checkout />} />
+                <Route path='/shop' element={<Shop />}/>
             </Routes>
         </BrowserRouter>
     );
